0c494bffb03e38d8aba3c675f246727befb47b5c
ZeeshanSalem <zeeshansaleem729@gmail.com>
2021-03-01 22:19:54 +0500

    templet is setup

---
+++ android/app/src/main/kotlin/com/example/dio_connectivity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dio_connectivity
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  Dio dio;
+  String firstPostTile;
+  bool isLoading;
+
+  @override
+  void initState() {
+    super.initState();
+    dio = Dio();
+    firstPostTile = 'Press the button 👇';
+    isLoading = false;
+
+//    dio.interceptors.add(
+//      RetryOnConnectionChangeInterceptor(
+//        requestRetrier: DioConnectivityRequestRetrier(
+//          dio: dio,
+//          connectivity: Connectivity(),
+//        ),
+//      ),
+//    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(),
+
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if(isLoading)
+              CircularProgressIndicator()
+            else
+              Text(firstPostTile,
+              style: TextStyle(
+                fontSize: 24
+              ),
+              textAlign: TextAlign.center,
+              ),
+
+            RaisedButton(
+                onPressed: (){
+                  setState(() {
+                    isLoading = true;
+                  });
+                },
+              child: Text("REQUEST DATA"),
+            ),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
